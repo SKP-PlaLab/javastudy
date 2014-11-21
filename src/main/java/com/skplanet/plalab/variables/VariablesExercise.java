6e505b0c61971e262f6f839e04a972dfff805f24
ez2sugul <seunghoon100@gmail.com>
2014-11-21 14:44:37 +0900

    add homework

---
+++ src/main/java/com/skplanet/plalab/variables/VariablesExercise.java
@@ -0,0 +1,14 @@
+package com.skplanet.plalab.variables;
+
+public class VariablesExercise {
+	
+	public static void main(String[] args) {
+		String myName = "백승훈";
+		String myPhoneNumber = "010-2011-9386";
+		int footSize = 275;
+		
+		System.out.println("My name is " + myName);
+		System.out.println("My phone number is " + myPhoneNumber);
+		System.out.println("My foot size is " + footSize);
+	}
+}
