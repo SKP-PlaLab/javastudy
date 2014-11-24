fd88c4f9b08850438b1e955feb186c907d74f8d5
ez2sugul <seunghoon100@gmail.com>
2014-11-24 14:35:02 +0900

    done operator exercise 3-7, 3-8

---
+++ src/main/java/com/skplanet/plalab/operator/OperatorExercise7.java
@@ -0,0 +1,13 @@
+package com.skplanet.plalab.operator;
+
+public class OperatorExercise7 {
+
+	public static void main(String[] args) {
+		int fahrenheit = 100;
+		float celcius = (float) (5.0/9 * (fahrenheit - 32.0f));
+		
+		System.out.println("fahrenheit = " + fahrenheit);
+		System.out.println("celcius = " + celcius);
+	}
+
+}
