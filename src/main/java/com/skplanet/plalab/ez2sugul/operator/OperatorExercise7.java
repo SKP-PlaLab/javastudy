01ecbc8a17e84592ca644cf06a90f69714548b70
ez2sugul <seunghoon100@gmail.com>
2014-11-24 17:50:42 +0900

    round to 2 decimal points

--- src/main/java/com/skplanet/plalab/ez2sugul/operator/OperatorExercise7.java
+++ src/main/java/com/skplanet/plalab/ez2sugul/operator/OperatorExercise7.java
@@ -4,7 +4,12 @@ public class OperatorExercise7 {
 
 	public static void main(String[] args) {
 		int fahrenheit = 100;
-		float celcius = (float) (5.0/9 * (fahrenheit - 32.0f));
+		float celcius =  (int) ((5.0/9 * (fahrenheit - 32.0f)) * 100 + 0.5f) / 100f;
+		
+//		celcius = celcius * 100f;
+//		celcius = celcius + 0.5f;
+//		celcius = (int) celcius;
+//		celcius = celcius / 100f;
 		
 		System.out.println("fahrenheit = " + fahrenheit);
 		System.out.println("celcius = " + celcius);
