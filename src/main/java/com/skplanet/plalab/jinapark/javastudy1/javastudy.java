d95dba8225f91dc43dbf97d2734c880f0f5dc10b
jinapark <mother0223@gmail.com>
2014-11-25 15:03:42 +0900

    처음

---
+++ src/main/java/com/skplanet/plalab/jinapark/javastudy1/javastudy.java
@@ -0,0 +1,17 @@
+package com.skplanet.plalab.jinapark.javastudy1;
+
+public class javastudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Parkjina" ;
+		String phonenumber = "010-3776-8223" ;
+		int footsize = 230 ;
+		
+		System.out.println("name = " + name);
+		System.out.println("phonenumber = " + phonenumber);
+		System.out.println("footsize = " + footsize);
+	}
+
+}
