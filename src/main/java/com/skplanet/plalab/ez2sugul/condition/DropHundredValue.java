255ca102fa84c39b023452e39b83518da7097249
ez2sugul <seunghoon100@gmail.com>
2014-12-05 18:43:06 +0900

    excercise 3-4 done

---
+++ src/main/java/com/skplanet/plalab/ez2sugul/condition/DropHundredValue.java
@@ -0,0 +1,8 @@
+package com.skplanet.plalab.ez2sugul.condition;
+
+public class DropHundredValue {
+	public static void main(String[] args) {
+		int num = 456;
+		System.out.println((int)(456 * 0.01d) * 100);
+	}
+}
