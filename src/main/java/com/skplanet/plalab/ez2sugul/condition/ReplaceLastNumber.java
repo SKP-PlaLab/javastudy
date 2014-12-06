dddcca12b0dc009f34b039e1c2a6c12916127de0
ez2sugul <seunghoon100@gmail.com>
2014-12-06 11:26:20 +0900

    Excercise 3-5 done

---
+++ src/main/java/com/skplanet/plalab/ez2sugul/condition/ReplaceLastNumber.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.skplanet.plalab.ez2sugul.condition;
+
+/**
+ * @author user
+ *
+ */
+public class ReplaceLastNumber {
+	public static void main(String[] args) {
+		int num = 335;
+		System.out.println((int)(num * 0.1) * 10 + 1);
+	}
+}
