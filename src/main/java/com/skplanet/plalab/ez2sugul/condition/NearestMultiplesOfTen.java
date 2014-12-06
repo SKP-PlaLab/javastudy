214a55a455f553663eb089770be468efefbcfed9
ez2sugul <seunghoon100@gmail.com>
2014-12-06 11:44:21 +0900

    Excercise 3-6 done

---
+++ src/main/java/com/skplanet/plalab/ez2sugul/condition/NearestMultiplesOfTen.java
@@ -0,0 +1,8 @@
+package com.skplanet.plalab.ez2sugul.condition;
+
+public class NearestMultiplesOfTen {
+	public static void main(String[] args) {
+		int num = 24;
+		System.out.println((int)(num * 0.1 + 0.9f) * 10 - num);
+	}
+}
