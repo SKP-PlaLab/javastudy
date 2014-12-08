ef1e814176e16f97cba9fa77a3b264980f1d8d2a
ez2sugul <seunghoon100@gmail.com>
2014-12-08 15:19:40 +0900

    test

---
+++ src/main/java/com/skplanet/plalab/ez2sugul/condition/ReplaceLastNumber.java
@@ -0,0 +1,11 @@
+package com.skplanet.plalab.ez2sugul.condition;
+
+public class ReplaceLastNumber {
+	public static void main(String[] args) {
+		int num = 333;
+		// 333 % 10 = 3
+		// 330 + 1
+		System.out.println((int)(num * 0.1d) * 100 + 1);
+		// 3300
+	}
+}
